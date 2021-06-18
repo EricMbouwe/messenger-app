fd428df31d8025118f3e0d1055ff0390ec063f4f
ericmbouwe <ericmbouwe@yahoo.fr>
2021-06-18 10:39:20 +0100

    add db connection test

--- server/db/db.js
+++ server/db/db.js
@@ -4,4 +4,8 @@ const db = new Sequelize(process.env.DATABASE_URL || "postgres://eric:0611@local
   logging: false
 });
 
+db.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.log('Error: ', err))
+
 module.exports = db;
